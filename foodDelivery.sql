9013f7fff26e447f6edcb1cec1c0ccf5f0096ccd
ask3403 <sm.abhishek34@gmail.com>
2022-10-06 19:23:44 +0530

    v3

--- foodDelivery.sql
+++ foodDelivery.sql
@@ -1,28 +1,28 @@
 select * from foodDelivery;
-update foodDelivery set Time_taken_min = substring(Time_taken_min,6,8);
+update foodDelivery set timeTaken = substring(timeTaken,6,8);
 
 -- PickupTime 
-ALTER TABLE foodDelivery ADD PickupTime int;
-update foodDelivery set PickupTime = DATEDIFF(minute,Time_Orderd, Time_Order_picked);
-update foodDelivery set PickupTime = 1440 + PickupTime where PickupTime < 0 ;
+ALTER TABLE foodDelivery ADD pickupTime int;
+update foodDelivery set pickupTime = DATEDIFF(minute,timeOrderd, timeOrderPicked);
+update foodDelivery set pickupTime = 1440 + pickupTime where pickupTime < 0 ;
 
 --delivered distance
 ALTER TABLE foodDelivery ADD deliveredDistance float;
-update foodDelivery set deliveredDistance =  SQRT(SQUARE(Delivery_location_latitude - Restaurant_latitude)
-+ SQUARE(Delivery_location_longitude - Restaurant_longitude));
+update foodDelivery set deliveredDistance =  SQRT(SQUARE(deliveryLocationLatitude - restaurantLatitude)
++ SQUARE(deliveryLocationLongitude - restaurantLongitude));
 DELETE from foodDelivery where deliveredDistance > 1;
 
 --finalizing table
 ALTER table foodDelivery 
 drop COLUMN 
-Delivery_location_latitude, Restaurant_latitude, Delivery_location_longitude,Restaurant_longitude, Time_Order_picked;
+deliveryLocationLatitude, restaurantLatitude, deliveryLocationLongitude,restaurantLongitude, timeOrderPicked;
 
 
-create TABLE AgeDeliveryCount ( ages VARCHAR(20), delivery_count int);
-select * from AgeDeliveryCount;
+create TABLE AgedDeliveryCount ( ages VARCHAR(20), deliveriesCount int);
+select * from AgedDeliveryCount;
 
-create TABLE totalTimeAndDistance ( city VARCHAR(20),type_of_vehicle VARCHAR(20), time_taken_min float, delivereddistance float);
+create TABLE totalTimeAndDistance ( city VARCHAR(20),typeOfVehicles VARCHAR(20), timeTaken float, deliveredDistance float);
 select * from totalTimeAndDistance;
 
-create TABLE totalVehicleCount (city VARCHAR(20),type_of_vehicle VARCHAR(20), cnt int);
-select * from totalVehicleCount;
+create TABLE totalVehiclesCount (city VARCHAR(20),typeOfVehicles VARCHAR(20), cnt int);
+select * from totalVehiclesCount;
