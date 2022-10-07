4abbf649c8fd85a91ed593e673313250d0b3ba37
ask3403 <sm.abhishek34@gmail.com>
2022-10-07 11:25:32 +0530

    v3

--- foodDelivery.sql
+++ foodDelivery.sql
@@ -1,21 +1,21 @@
 select * from foodDelivery;
-update foodDelivery set timeTaken = substring(timeTaken,6,8);
+update foodDelivery set time_taken = substring(time_taken,6,8);
 
 -- PickupTime 
-ALTER TABLE foodDelivery ADD pickupTime int;
-update foodDelivery set pickupTime = DATEDIFF(minute,timeOrderd, timeOrderPicked);
-update foodDelivery set pickupTime = 1440 + pickupTime where pickupTime < 0 ;
+ALTER TABLE foodDelivery ADD pickup_time int;
+update foodDelivery set pickup_time = DATEDIFF(minute,time_orderd, time_order_picked);
+update foodDelivery set pickup_time = 1440 + pickup_time where pickup_time < 0 ;
 
 --delivered distance
-ALTER TABLE foodDelivery ADD deliveredDistance float;
-update foodDelivery set deliveredDistance =  SQRT(SQUARE(deliveryLocationLatitude - restaurantLatitude)
-+ SQUARE(deliveryLocationLongitude - restaurantLongitude));
-DELETE from foodDelivery where deliveredDistance > 1;
+ALTER TABLE foodDelivery ADD delivered_distance float;
+update foodDelivery set delivered_distance =  SQRT(SQUARE(delivery_location_latitude - restaurant_latitude)
++ SQUARE(delivery_location_longitude - restaurant_longitude));
+DELETE from foodDelivery where delivered_distance > 1;
 
 --finalizing table
 ALTER table foodDelivery 
 drop COLUMN 
-deliveryLocationLatitude, restaurantLatitude, deliveryLocationLongitude,restaurantLongitude, timeOrderPicked;
+delivery_location_latitude, restaurant_latitude, delivery_location_longitude,restaurant_longitude, time_order_picked;
 
 
 create TABLE AgedDeliveryCount ( ages VARCHAR(20), deliveriesCount int);
